23d21a7ee0f64a9e05853fd1f7bec61b86647928
journeytorainbow <58691899+journeytorainbow@users.noreply.github.com>
2020-07-01 22:39:15 +0900

    Refactor food and bomb to Class

--- game_oop.py
+++ game_oop.py
@@ -29,6 +29,25 @@ Font3 = pygame.font.SysFont('Impact', 25)
 
 record = []
 
+class Item:
+    def __init__(self):
+        self.position = []
+    
+    def add_item(self, snake):
+        while True:
+            pos = (random.randint(0, x_box - 1), random.randint(0, y_box - 1))
+            if pos in self.position or pos in snake.bodies:
+                continue
+            self.position.append(pos)
+            break
+
+    def draw(self, color):
+        for pos in self.position:
+                pygame.draw.ellipse(screen, color, Rect((pos[0]*x_length, pos[1]*y_length), (x_length, y_length)))
+    
+food = Item()
+bomb = Item()
+
 class Snake:
     def __init__(self, pos):
         self.bodies = [pos]
@@ -45,47 +64,29 @@ class Snake:
             ypos += 1
         head = (xpos, ypos)
 
-        game_over = head in bombs or head in self.bodies or head[0] < 0 or head[1] < 0 or head[0] >= x_box or head[1] >= y_box
+        game_over = head in bomb.position or head in self.bodies or head[0] < 0 or head[1] < 0 or head[0] >= x_box or head[1] >= y_box
 
         self.bodies.insert(0, head)
-        if head in foods:
-            foods.remove(head)
-            add_food(self)
-        elif head in bombs:
-            bombs.remove(head)
+        if head in food.position:
+            food.position.remove(head)
+            food.add_item(self)
+        elif head in bomb.position:
+            bomb.position.remove(head)
             self.bodies.pop()
         else:
             self.bodies.pop()
         
         return game_over
 
-    def draw(self):
+    def draw(self, color):
         for body in self.bodies:
-            pygame.draw.rect(screen, MINT, Rect(body[0]*x_length, body[1]*y_length, x_length, y_length))
-
-def add_food(snake):
-    while True:
-        pos = (random.randint(0, x_box - 1), random.randint(0, y_box - 1))
-        if pos in foods or pos in snake.bodies:
-            continue
-        foods.append(pos)
-        break
-
-def add_bomb(snake):
-    while True:
-        pos = (random.randint(0, x_box - 1), random.randint(0, y_box - 1))
-        if pos in bombs or pos in foods or pos in snake.bodies:
-            continue
-        bombs.append(pos)
-        break
-
-def playing_paint(snake):
+            pygame.draw.rect(screen, color, Rect(body[0]*x_length, body[1]*y_length, x_length, y_length))
+    
+def playing_paint(snake, food, bomb):
     screen.fill(0)
-    snake.draw()
-    for food in foods:
-        pygame.draw.ellipse(screen, PINK, Rect((food[0]*x_length, food[1]*y_length), (x_length, y_length)))
-    for bomb in bombs:
-        pygame.draw.ellipse(screen, GREEN, Rect((bomb[0]*x_length, bomb[1]*y_length), (x_length, y_length)))
+    snake.draw(MINT)
+    food.draw(PINK)
+    bomb.draw(GREEN)
     for xpos in range(20):
         pygame.draw.line(screen, LINE_COLOR, (xpos*x_length, 0), (xpos*x_length, height))        
     for ypos in range(20):
@@ -117,9 +118,9 @@ def main():
     num_food = 20
     num_bomb = 3
     for _ in range(num_food):
-        add_food(snake)
+        food.add_item(snake)
     for _ in range(num_bomb):
-        add_bomb(snake)
+        bomb.add_item(snake)
     while running:
         time_past = pygame.time.get_ticks() - time_start
         time_render = Font3.render(str(int((time_past)/60000)) + ":" + str(int((time_past)/1000%60)).zfill(2), True, WHITE)
@@ -137,7 +138,7 @@ def main():
             msg2 = Font2.render("Try again![press spacebar]", True, WHITE)
             running = False
         
-        playing_paint(snake)
+        playing_paint(snake, food, bomb)
         time_paint(time_render)
         fpsClock.tick(FPS)
 
@@ -153,6 +154,6 @@ while True:
             pygame.quit()
             sys.exit()
         elif event.type == KEYDOWN and event.key == K_SPACE:
-            foods = []
-            bombs = []
+            food.position = []
+            bomb.position = []
             main() # replay
